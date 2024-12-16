acc4489300ca49de680c881f350a10e631bf5d76
dlsqhrvv <dlsqhrvv@gmail.com>
2024-12-16 11:59:38 +0900

    feat : fibonach is maked binfeat : fibonach is maked bin

--- fibo.py
+++ fibo.py
@@ -1,8 +1,18 @@
-def fibo(num):
-    if num<2:
-        return num
-    else:
+#def fibo(num):
+#    if num<2:
+#        return num
+#    else:
         
-        return fibo(num-1)+fibo(num-2)
+#        return fibo(num-1)+fibo(num-2)
 
-print(fibo(6)) 
+#print(fibo(5)) 
+
+from math import sqrt, pow
+
+def fibo(num):
+    phi = (1 + sqrt(5)) / 2
+    return round(pow(phi, num) / sqrt(5))
+
+print(fibo(5))
+
+
